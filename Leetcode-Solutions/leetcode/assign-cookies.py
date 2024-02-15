5de802fd306aa3fb12db4080bb50c0f405c3d669
SimonDerejeW <134320950+SimonDerejeW@users.noreply.github.com>
2024-02-15 22:41:39 +0300

    Add solution for Assign Cookies

---
+++ Leetcode-Solutions/leetcode/assign-cookies.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        left = right = 0
+        count = 0
+        g.sort()
+        s.sort()
+        while left < len(g) and right < len(s):
+            if g[left] <= s[right]:
+                count += 1
+                left += 1
+            right += 1
+            
+
+        return count
+
