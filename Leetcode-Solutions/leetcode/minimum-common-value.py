ae6cec3b2b5270a4205b4558fc73b015bbec0a8a
SimonDerejeW <134320950+SimonDerejeW@users.noreply.github.com>
2024-02-15 22:40:46 +0300

    Add solution for Minimum Common Value

---
+++ Leetcode-Solutions/leetcode/minimum-common-value.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        l, r = 0,0
+        while l < len(nums1) and r < len(nums2):
+            if nums1[l] == nums2[r]:
+                return nums1[l]
+            elif nums1[l] < nums2[r]:
+                l+=1
+            else:
+                r+=1
+                
+        return -1
+        
