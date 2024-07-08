324b9bae9191bdfa70c98c92b5ce28f65aeb8cfe
SimonDerejeW <134320950+SimonDerejeW@users.noreply.github.com>
2024-07-08 21:03:30 +0300

    Created 'A - Your Hackathon Project - Chat Feature' by A2SV Hub

---
+++ A2SV G5 - Contest #24 08-Jul-2024/A - Your Hackathon Project - Chat Feature 177836.py
@@ -0,0 +1,15 @@
+# Problem: A - Your Hackathon Project - Chat Feature - https://codeforces.com/gym/534160/problem/A
+
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    count = 0
+    for i in range(len(s) - 1,-1,-1):
+        if s[i]==')':
+            count += 1
+        else:
+            break
+    if count > (len(s) - count):
+        print("Yes")
+    else:
+        print("No")
