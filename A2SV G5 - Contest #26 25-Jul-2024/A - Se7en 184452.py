a3889c5ecd39ca7e0446857e83b658d90407f148
SimonDerejeW <134320950+SimonDerejeW@users.noreply.github.com>
2024-07-25 12:49:18 +0300

    Created 'A - Se7en' by A2SV Hub

---
+++ A2SV G5 - Contest #26 25-Jul-2024/A - Se7en 184452.py
@@ -0,0 +1,15 @@
+# Problem: A - Se7en - https://codeforces.com/gym/537362/problem/A
+
+for _ in range(int(input())):
+    n = int(input())
+    s = str(n)
+    if n % 7 == 0:
+        print(n)
+    else:
+        s = list(s)
+        s[-1] = "0"
+        x = int("".join(s))
+        for i in range(x, x+11):
+            if i % 7 == 0:
+                print(i)
+                break
