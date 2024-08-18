b917463da110648c11b662e2ab4a16df6735e122
SimonDerejeW <134320950+SimonDerejeW@users.noreply.github.com>
2024-08-18 13:11:00 +0300

    Created 'B - Fasika and Children' by A2SV Hub

---
+++ A2SV G5 - Contest #27 18-Aug-2024/B - Fasika and Children 197597.py
@@ -0,0 +1,21 @@
+# Problem: B - Fasika and Children - https://codeforces.com/gym/538762/problem/B
+
+from collections import deque
+
+
+n , m = list(map(int, input().split()))
+nums = list(map(int, input().split()))
+
+que = deque()
+
+for i in range(len(nums)):
+    que.append([nums[i] , i+1])
+
+last = n
+while que:
+    x, idx = que.popleft()
+    x-=m
+    if x > 0:
+        last = idx
+        que.append([x,idx])
+print(last)
