e17b56099bca7292300381af43c09184862c3a29
SimonDerejeW <134320950+SimonDerejeW@users.noreply.github.com>
2024-08-18 13:40:36 +0300

    Created 'B - Optimal Point' by A2SV Hub

---
+++ A2SV G5 - Contest #25 18-Aug-2024/B - Optimal Point 197599.py
@@ -0,0 +1,20 @@
+# Problem: B - Optimal Point - https://codeforces.com/gym/535749/problem/B
+
+from collections import defaultdict
+
+
+for _ in range(int(input())):
+    n , k = list(map(int, input().split()))
+    left = False
+    right = False
+    for i in range(n):
+        l,r = list(map(int, input().split()))
+        if l == k:
+            left = True
+        if r == k:
+            right = True
+    if left and right:
+        print("YES")
+    else:
+        print("NO")
+    
