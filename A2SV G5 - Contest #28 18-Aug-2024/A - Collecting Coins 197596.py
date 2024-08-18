2000aa1101b6576942fe1c708fab3973d61955db
SimonDerejeW <134320950+SimonDerejeW@users.noreply.github.com>
2024-08-18 13:01:07 +0300

    Created 'A - Collecting Coins' by A2SV Hub

---
+++ A2SV G5 - Contest #28 18-Aug-2024/A - Collecting Coins 197596.py
@@ -0,0 +1,13 @@
+# Problem: A - Collecting Coins - https://codeforces.com/gym/540354/problem/A
+
+
+for _ in range(int(input())):
+    a , b ,c , n = list(map(int, input().split()))
+    diff = (max(a,b,c) - a) + (max(a,b,c) - b) + (max(a,b,c) - c)
+    diff = n - diff
+    ans = diff >= 0 and diff % 3 == 0
+    if ans:
+        print("YES")
+    else:
+        print("NO")
+    
